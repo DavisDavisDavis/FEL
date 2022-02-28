ccc984dcd2811637b98d7614c8de1a728e88c52e
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-02-28 15:16:13 +0100

    added header + body styling

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 import Darkmode from "darkmode-js";
 
 new Darkmode().showWidget();
+
+const options = {
+  time: "0.5s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "#DFE2D8", // default: '#fff'
+  buttonColorDark: "#100f2c", // default: '#100f2c'
+  buttonColorLight: "#fff", // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: "🌓", // default: ''
+  autoMatchOsTheme: true, // default: true
+};
+
+const darkmode = new Darkmode(options);
