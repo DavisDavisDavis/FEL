90e2f7fa37585513229730babe5ba9b2faeddf52
DavisDavisDavis <olizoooom@gmail.com>
2022-02-28 13:07:21 +0100

    fixed vite by deleting everythin 😢

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-import "./style.css";
+import './style.css'
 
-import Splide from "@splidejs/splide";
-var splide = new Splide(".splide", {
-  type: "loop",
-  perPage: 3,
-  focus: "center",
-});
-
-splide.mount();
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
