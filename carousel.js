8f57777e2977ae92b92e70065f75f38a7a20294d
DavisDavisDavis <89775852+DavisDavisDavis@users.noreply.github.com>
2022-03-02 21:16:00 +0100

    carousel  mobile is almost done

--- carousel.js
+++ carousel.js
@@ -1,24 +1,19 @@
 import Splide from "@splidejs/splide";
 
-function resizeListener() {
-  return window.innerWidth;
-}
-
-window.addEventListener("resize", resizeListener);
-
-let pages = 5;
-if (resizeListener() < 1250) {
-  pages = 3;
-} else {
-  pages = 5;
-}
-
 new Splide(".splide").mount();
 
 var splide = new Splide(".splide", {
   type: "loop",
-  perPage: pages,
-  focus: "center",
+  perPage: 5,
+  breakpoints: {
+    1250: {
+      perPage: 3,
+    },
+    750: {
+      perPage: 2,
+    },
+    focus: "center",
+  },
 });
 
 splide.mount();
