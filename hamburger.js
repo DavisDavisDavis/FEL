da2c0d65a6b5e3d63a7244707cf78c9e25f0da93
DavisDavisDavis <olizoooom@gmail.com>
2022-03-03 19:09:52 +0100

    hamburger stuff

---
+++ hamburger.js
@@ -0,0 +1,30 @@
+const bodyTag = document.querySelector('body');
+const menuToggle = document.querySelector('a.menu-toggle');
+const hamburgerIcon = document.querySelector('a.menu-toggle svg');
+const menu = document.querySelector('.menu-hamburger')
+
+
+// Hamburger toggle
+
+// Listen for click
+menuToggle.addEventListener('click', () => {
+    bodyTag.classList.toggle('nav-open');
+    menu.classList.toggle('menu-open');
+    console.log('open hamburger menu!');
+
+    // Change svg icon to closed when opened
+    if (bodyTag.classList.contains('nav-open')) {
+        gsap.to(".burger-top", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: 7 })
+        gsap.to(".burger-middle", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: -3 })
+        gsap.to(".burger-bottom", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: -10 })
+        gsap.to(".burger-top", {ease: 'power4.out', rotation: 45, transformOrigin: "50% 50%", delay: 0.25 })
+        gsap.to(".burger-middle", {ease: 'power4.out', rotation: 45, transformOrigin: "50% 50%", delay: 0.25 })
+        gsap.to(".burger-bottom", {ease: 'power4.out', rotation: -45, transformOrigin: "50% 50%", delay: 0.25 })
+    } else {
+        gsap.to(".burger-top", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: 7 })
+        gsap.to(".burger-middle", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: 0 })
+        gsap.to(".burger-bottom", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: -10 })
+        gsap.to(".burger-top", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: 0, delay: 0.25 })
+        gsap.to(".burger-bottom", {ease: 'power4.out', rotation: 0, transformOrigin: "50% 50%", y: 0, delay: 0.25 })
+    }
+});
