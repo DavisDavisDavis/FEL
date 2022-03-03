3bdcbbcfe33b76d951083346ee526161e639682e
agnesskonvall <89479983+agnesskonvall@users.noreply.github.com>
2022-03-03 14:55:37 +0100

    added rocket button

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-import Darkmode from "darkmode-js";
-
-new Darkmode().showWidget();
-
-const options = {
-  time: "0.5s", // default: '0.3s'
-  mixColor: "#fff", // default: '#fff'
-  backgroundColor: "#DFE2D8", // default: '#fff'
-  buttonColorDark: "#100f2c", // default: '#100f2c'
-  buttonColorLight: "#fff", // default: '#fff'
-  saveInCookies: false, // default: true,
-  label: "🌓", // default: ''
-  autoMatchOsTheme: true, // default: true
-};
-
-const darkmode = new Darkmode(options);
+const button = document.getElementById("rocket");
+button.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
